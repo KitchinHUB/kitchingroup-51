7134d1517035ec5838447f75fb4bd3c5f7d15154
John Kitchin <jkitchin@andrew.cmu.edu>
2016-02-07 17:56:08 -0500

    init

---
+++ kitchingroup-51.el
@@ -0,0 +1,39 @@
+;;; kitchingroup-51.el --- Core-level shifts in Cu-Pd alloys as a function of bulk composition and structure
+
+;; Copyright (C) 2016 John Kitchin
+
+;; Version: 0.0.1
+;; Author: Jacob R. Boes
+;;   Peter Kondratyuk
+;;   Chunrong Yin
+;;   James B. Miller
+;;   Andrew J. Gellman
+;;   John Kitchin <jkitchin@andrew.cmu.edu>
+;; Keywords:
+;; DOI: 10.1016/j.susc.2015.02.011
+;; Journal: Surface Science
+;; Bibtex: @article{boes-2015-core-cu,
+;;   author =	 {Jacob R. Boes and Peter Kondratyuk and Chunrong Yin and James
+;;                   B. Miller and Andrew J. Gellman and John R. Kitchin},
+;;   title =	 {Core Level Shifts in {Cu-Pd} Alloys As a Function of Bulk
+;;                   Composition and Structure},
+;;   journal =	 {Surface Science},
+;;   volume =	 640,
+;;   pages =	 {127-132},
+;;   year =	 2015,
+;;   doi =	 {10.1016/j.susc.2015.02.011},
+;;   url =	 {http://dx.doi.org/10.1016/j.susc.2015.02.011},
+;;   keywords =	 {DESC0004031, early-career},
+;;   issn =	 "0039-6028",
+;;  }
+
+;;; Commentary:
+
+;;; Code:
+(require 'cappa)
+
+(cappa-register 'kitchingroup-51)
+
+
+(provide 'kitchingroup-51)
+;;; kitchingroup-51.el ends here
